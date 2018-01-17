45638aeacdbddc77c734f05b1b50508cc22158d5
ryne.minor <ryne.minor@gmail.com>
2018-01-17 17:58:36 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  
+  get 'static_pages/about'
+  
+  get 'static_pages/help'
+  
+  resources :quotes
+  
+  root 'static_pages#home'
+end
